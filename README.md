925254e81308f913386547c50261a9cf11a0e7cd
Jordan Matelsky <j6k4m8@gmail.com>
2016-02-23 20:43:23 -0500

    update to use settings file

---
+++ README.md
@@ -0,0 +1,6 @@
+# Gmail Checker
+
+Uses selenium (`pip install selenium`) in headless mode (`brew install phantomjs
+`) to automatically check your external mailboxes from within Gmail, which is dumb and takes *5eva* to do it automatically.
+
+I run this as a cron job as a server, but your results may vary.
--- check.py
+++ check.py
@@ -6,18 +6,22 @@ from selenium.webdriver.support import expected_conditions as EC
 import settings
 
 driver = webdriver.Chrome()
+# driver.set_window_size(1120, 550)
 driver.implicitly_wait(10)
+wait = WebDriverWait(driver, 10)
+
+print("Loading login page...")
 driver.get("https://accounts.google.com/ServiceLogin?service=mail#identifier")
 usernametext = driver.find_element_by_name('Email')
 usernametext.send_keys(settings.USERNAME)
 driver.find_element_by_name('signIn').click()
-wait = WebDriverWait(driver, 10)
-wait.until(EC.element_to_be_clickable((By.ID,'Passwd')))
 passwordtext = driver.find_element_by_name('Passwd') \
                      .send_keys([settings.PASSWORD, Keys.RETURN])
+print("Signed in. Going to accounts page...")
 wait.until(EC.element_to_be_clickable((By.ID,':29')))
 driver.get("https://mail.google.com/mail/u/0/#settings/accounts")
 links = driver.find_elements_by_xpath("//*[contains(text(), 'Check mail now')]")
+print("Clicking links...")
 for l in links:
     try:
         l.click()
@@ -29,4 +33,5 @@ for l in links:
         l.click()
     except:
         pass
-# driver.close()
+print("Done.")
+driver.close()
