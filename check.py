aa3f997b35270b612336e867b23a4fe64691dee3
Jordan Matelsky <j6k4m8@gmail.com>
2016-05-12 17:35:52 -0400

    updates to the selenium driver

--- check.py
+++ check.py
@@ -8,6 +8,7 @@ import time
 import settings
 
 driver = webdriver.PhantomJS()
+driver = webdriver.Chrome()
 driver.set_window_size(1120, 550)
 #driver.manage().wndow.maximize()
 driver.implicitly_wait(50)
@@ -16,15 +17,15 @@ wait = WebDriverWait(driver, 20)
 print("Loading login page...")
 driver.get("https://accounts.google.com/ServiceLogin?service=mail#identifier")
 driver.set_window_size(1000, 800);
-time.sleep(2); usernametext = driver.find_element_by_name('Email')
-time.sleep(1); usernametext.send_keys(settings.USERNAME)
-time.sleep(2); driver.find_element_by_name('signIn').click()
-time.sleep(5); passwordtext = driver.find_element_by_name('Passwd') \
+usernametext = driver.find_element_by_name('Email')
+usernametext.send_keys(settings.USERNAME)
+driver.find_element_by_name('signIn').click()
+passwordtext = driver.find_element_by_name('Passwd') \
                       .send_keys([settings.PASSWORD, Keys.RETURN])
-time.sleep(6);print("Signed in. Going to accounts page...")
-#time.sleep(5); wait.until(EC.element_to_be_clickable((By.ID,':29')))
-time.sleep(3); driver.get("https://mail.google.com/mail/u/0/#settings/accounts")
-time.sleep(3); links = driver.find_elements_by_xpath("//*[contains(text(), 'Check mail now')]")
+print("Signed in. Going to accounts page...")
+#wait.until(EC.element_to_be_clickable((By.ID,':29')))
+driver.get("https://mail.google.com/mail/u/0/#settings/accounts")
+links = driver.find_elements_by_xpath("//*[contains(text(), 'Check mail now')]")
 print("Clicking links...")
 cs = 0
 for l in links:
